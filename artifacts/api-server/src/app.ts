7f2b407007830aca26348210eb795755d7e02f8d
Karel <karaps5705@gmail.com>
2026-03-20 00:32:03 +0100

    fix: remove options star Express 5

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -13,7 +13,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type","Authorization"]
 }));
 
-app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
